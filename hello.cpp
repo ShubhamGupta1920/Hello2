4a38b5c0589969f7274a4892cb32e36a36834c8c
Shubh <shubhamkumargupta323@gmail.com>
2025-12-04 20:58:48 +0530

    5th

--- hello.cpp
+++ hello.cpp
@@ -2,21 +2,8 @@
 using namespace std;
 int main(){
     cout<<"hello"<<endl;
-<<<<<<< HEAD
     cout<<"Jigyasha"<<endl;
-=======
-
-    int c;
-    cout<<"Shubham Gupta"<<endl;
-    //add two numbers
-
-
-    int a=10,b=20;
-    cout<<a+b;
-    cout<<"Hello Shubham"<<endl;
-    cout<<"Hello How are you !"<<endl;
-
->>>>>>> main
+    cout<<"Jigyasha"<<endl;
     
     
 
